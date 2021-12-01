b7146c7245581cf84885931db4f43f3aaf549dce
Vitor Honna <vitorhonna@gmail.com>
2021-12-01 00:25:49 -0300

    Adapt profile to receive data from the backend

--- src/routes.js
+++ src/routes.js
@@ -3,10 +3,19 @@ const routes = express.Router();
 
 const views = __dirname + '/views';
 
+const profile = {
+    name: 'Vitor',
+    avatar: 'https://avatars.githubusercontent.com/u/19689158?v=4',
+    'monthly-budget': 5000,
+    'days-per-week': 5,
+    'hours-per-day': 8,
+    'vacation-per-year': 4,
+};
+
 // request, response
 routes.get('/', (req, res) => res.render(`${views}/index`));
 routes.get('/job', (req, res) => res.render(`${views}/job`));
 routes.get('/job/edit', (req, res) => res.render(`${views}/job-edit`));
-routes.get('/profile', (req, res) => res.render(`${views}/profile`));
+routes.get('/profile', (req, res) => res.render(`${views}/profile`, { profile }));
 
 module.exports = routes;
